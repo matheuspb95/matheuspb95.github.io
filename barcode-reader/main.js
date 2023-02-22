e1d5d96e1683495b5d2a99229d00496fef9ae2d2
matheus.barbosa <matheus.barbosa@itriad.org.br>
2023-02-22 11:03:29 -0400

    Adicionado função de copiar texto

--- barcode-reader/main.js
+++ barcode-reader/main.js
@@ -1,3 +1,5 @@
+const codeValue = document.getElementById("codvalue");
+
 // Get the video element
 const video = document.querySelector("#video");
 // Check if device has camera
@@ -22,7 +24,6 @@ BarcodeDetector.getSupportedFormats().then((arr) => (formats = arr));
 // Create new barcode detector with all supported formats
 const barcodeDetector = new BarcodeDetector({ formats });
 // const barcodeDetector = new BarcodeDetector("itf");
-const div = document.getElementById("results");
 // Detect code function
 const detectCode = () => {
   // Start detecting codes on to the video element
@@ -36,17 +37,35 @@ const detectCode = () => {
         // Log the barcode to the console
         console.log(barcode);
         if (barcode["rawValue"].length == 44) {
-          div.innerHTML =
-            "Result: " + barcode["rawValue"] + "\nFormat: " + barcode["format"];
+          codeValue.value = barcode["rawValue"];
         }
       }
     })
     .catch((err) => {
       // Log an error if one happens
       console.error(err);
-
-      div.innerHTML += "";
     });
 };
 
 setInterval(detectCode, 100);
+
+function showSnackBar(textMsg) {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+  x.innerHTML = textMsg;
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+const copyCode = () => {
+  codeValue.select();
+  codeValue.setSelectionRange(0, 99999); // For mobile devices
+  
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(codeValue.value);
+  showSnackBar("Código copiado: " + codeValue.value);
+}
